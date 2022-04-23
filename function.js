f62ef3cf9c0eac761906bba32f8853101db4d95a
Undefineid <root@rootless.fr>
2022-04-23 14:26:22 +0200

    First push

---
+++ function.js
@@ -0,0 +1,42 @@
+var xa = "";
+var xy = "";
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+async function loadValue(){
+  xa = Math.round(getRandomArbitrary(42.3327778, 51.0716667)*10000000)/10000000;
+  xy = Math.round(getRandomArbitrary(-4.795555555555556, 8.230555555555556)*10000000000000000)/10000000000000000;
+
+  while(await rtn() != 'France'){
+    xa = Math.round(getRandomArbitrary(42.3327778, 51.0716667)*10000000)/10000000;
+    xy = Math.round(getRandomArbitrary(-4.795555555555556, 8.230555555555556)*10000000000000000)/10000000000000000;
+  }
+  document.getElementById('cds').innerHTML = "Coordonnées : " + xa + "," + xy;
+}
+
+async function rtn(){
+  const geocoder = new google.maps.Geocoder();
+  return await geocoder.geocode({ location: {lat: xa, lng: xy} }).then((response) => {
+      for (a of response.results[0].address_components){
+        if(a.types[0] == "country"){
+          return (a.long_name);
+        }
+      }
+      return "";
+    });
+}
+
+async function loadMap() {
+  await loadValue();
+  map = new google.maps.Map(document.getElementById("main"), {
+    center: { lat: xa, lng: xy },
+    zoom: 8,
+  });
+  new google.maps.Marker({
+    position: { lat: xa, lng: xy },
+    map,
+    title: "Ici",
+  });
+}
