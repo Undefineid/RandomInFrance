fff4bfb576d4effb55abd497e2ed70b447dbfa70
Undefineid <root@rootless.fr>
2022-04-23 17:03:10 +0200

    Func sleep Ok

--- function.js
+++ function.js
@@ -5,6 +5,8 @@ function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+const sleep = (delay) => new Promise( (resolve) => setTimeout(resolve, delay));
+
 function generateValue(){
   xa = Math.round(getRandomArbitrary(423327778, 510716667))/10000000;
   xy = Math.round(getRandomArbitrary(-4795555555555556, 8230555555555556))/1000000000000000;
@@ -24,7 +26,7 @@ async function loadValue(){
   generateValue();
   while(await verifyCountry() != 'France'){
     generateValue();
-    sleep(1000);
+    await sleep(1000);
   }
   document.getElementById('cds').innerHTML = "Coordonnées : " + xa + "," + xy;
 }
